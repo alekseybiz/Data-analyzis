c494df2a9a3fbad5cf038c00c7ffc8c4f78fcdde
Alex007-Biz <bizukin@yandex.ru>
2024-05-23 08:39:13 +0300

    2

---
+++ AZ01_Numpy_Pandas/1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# data = [1, 2, 3, 4, 5]
+# series = pd.Series(data)
+# print(series)
+
+data = {
+    'Name' : ['Alice', 'Bob', 'Roma', 'Anna'],
+    'Age' : [23, 45, 17, 20],
+    'City' : ['New York', 'LA', 'Chicago', 'Moscow']
+}
+
+df = pd.DataFrame(data)
+print(df)
---
+++ AZ01_Numpy_Pandas/2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv('World-happiness-report-2024.csv')
+# print(df.tail(5))
+# print(df.info())
+# print(df.describe())
+# print(df[['Country name', 'Regional indicator']])
+print(df[df['Healthy life expectancy'] > 0.7])
