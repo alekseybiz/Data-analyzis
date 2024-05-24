b403fea45c162f41241c45366a12c5de203fdb7d
Alex007-Biz <bizukin@yandex.ru>
2024-05-24 08:09:34 +0300

    4

--- AZ01_Numpy_Pandas/4.py
+++ AZ01_Numpy_Pandas/4.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+df = pd.read_csv('animal.csv')
+print(df)
+
+# df.fillna(0, inplace=True)
+# df.dropna(inplace=True)
+df.drop(7, axis=0, inplace=True)
+print(df)
+group = df.groupby('Пища')['Средняя продолжительность жизни'].mean()
+
+print(group)
+
+df.to_csv('output_animal.csv', index=False)
