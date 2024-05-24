07060cf79fd26216b5b604dbfcedd38b83e282a2
Alex007-Biz <bizukin@yandex.ru>
2024-05-24 09:00:30 +0300

    hw

--- AZ01_Numpy_Pandas/homeWork.py
+++ AZ01_Numpy_Pandas/homeWork.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('amazon_reviews.csv')
-print(df)
+df = pd.read_csv('animal.csv')
+# print(df)
+df2 = pd.DataFrame(df)
+print(df2.head(5))
+print(df2.info())
+print(df2.describe())
+
+# print(df2)
