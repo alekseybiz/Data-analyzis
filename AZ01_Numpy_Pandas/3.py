e029961d21ee1ca680cbfd39186d215e9140ea06
Alex007-Biz <bizukin@yandex.ru>
2024-05-24 07:50:24 +0300

    Update 3.py

--- AZ01_Numpy_Pandas/3.py
+++ AZ01_Numpy_Pandas/3.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('liustry.csv')
+df = pd.read_csv('hh.csv')
 
-df['Test'] = [new for new in range(200)]
+df['Test'] = [new for new in range(27)]
 
 print(df)
+# df.drop('Test', axis=1, inplace=True)
+df.drop(26, axis=0, inplace=True)
+
+print(df)
+
