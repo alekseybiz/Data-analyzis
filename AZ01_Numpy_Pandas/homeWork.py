14af8349c3e9a1342bb98be16f07f31c7ad1c728
Alex007-Biz <bizukin@yandex.ru>
2024-05-30 08:03:35 +0300

    1

--- AZ01_Numpy_Pandas/homeWork.py
+++ AZ01_Numpy_Pandas/homeWork.py
@@ -3,8 +3,14 @@ import pandas as pd
 df = pd.read_csv('animal.csv')
 # print(df)
 df2 = pd.DataFrame(df)
-print(df2.head(5))
+print(df2.head(15))
 print(df2.info())
 print(df2.describe())
+
+value = df.iloc[0, 0]
+print(value)
+
+column_data = df['Животное']
+print(column_data)
 
 # print(df2)
