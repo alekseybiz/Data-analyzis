b9d1e2988bf61bc371defaa37838c68322a65312
Alex007-Biz <bizukin@yandex.ru>
2024-06-03 20:21:31 +0300

    3

--- Prices_Tiles_Competitors/price_analyzes_selenium.py
+++ Prices_Tiles_Competitors/price_analyzes_selenium.py
@@ -4,22 +4,31 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+# import requests
 
 df = pd.read_csv('sources_prices.csv')
-
 browser = webdriver.Chrome()
 
+# №1
 url = "https://www.plitkanadom.ru/collections/rossiiskaya-plitka/azori/eclipse/azori-eclipse-indigo-nastennaya-plitka-50-5x20-1"
 browser.get(url)
-# time.sleep(5)
 # Явное ожидание появления элемента
 element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.new-price')))
-
-# element = browser.find_element_by_css_selector('span.new-price')
-
 price = element.text
+print(url)
 print(price)
-browser.quit()
+# browser.quit()
+
+# №2
+url = "https://santehnika-online.ru/product/plitka_nastennaya_azori_eclipse_indigo_glyantsevaya/351699/"
+browser.get(url)
+# Явное ожидание появления элемента
+element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.b-price__price-core')))
+price = element.text
+print(url)
+print(price)
+
+
 
 
 
