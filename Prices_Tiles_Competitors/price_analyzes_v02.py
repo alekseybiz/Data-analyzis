0c8bb875806a7aea01ca3f6f6beb9784da2aecb8
Aleksey <bizukin@yandex.ru>
2024-09-18 16:54:35 +0300

    2

---
+++ Prices_Tiles_Competitors/price_analyzes_v02.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+df = pd.read_csv('sources_prices.csv')
+num_columns = len(df.columns)
+print(f'Количество столбцов в файле CSV: {num_columns}')
+num_rows = df.shape[0]
+print(f'Число строк в DataFrame: {num_rows}')
+browser = webdriver.Chrome()
+
+for col in range(1, num_columns):
+    print(f'Колонка: {col}')
+    tag = df.iloc[0, col]
+    name = df.iloc[1, col]
+    tag_name = tag + "." + name
+    print(f'Используемый тег: {tag_name}')
+    # print(tag_name)
+
+    for row in range(2, num_rows):
+        print(f'Ряд: {row}')
+        # row = 3
+        item_name = df.iloc[row, 0]
+        print(f'Товар: {item_name}')
+        url = df.iloc[row, col]
+
+        try:
+            browser.get(url)
+            # Явное ожидание появления элемента
+            element = WebDriverWait(browser, 12).until(EC.presence_of_element_located((By.CSS_SELECTOR, tag_name)))
+            price = element.text
+        except Exception as e:
+            # Обработка любого исключения
+            print("Произошла ошибка:", e)
+            price = False
+        print(url)
+        print(price)
+        df.iloc[row, col] = price
+
+# print(df)
+df.to_csv('out_prices.csv', index=False)
+df.to_excel('Prices_xls.xlsx', index=False, engine='openpyxl')
+
+print("Данные успешно сохранены в файл 'Prices_xls.xlsx'")
+
+#Если не получается парсинг, то пробуем вручную:
+# tag_name = "p.card-price"
+# url = 'https://3dplitka.ru/product-869368/'
+# browser.get(url)
+# element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, tag_name)))
+# # price = element.get_attribute('textContent')
+# price = element.text
+# print(price)
+
+browser.quit()
+
+
+
+
+
+
+
+
