23f55a34b5e5699965bb50cba1e824096f364b2d
Alex007-Biz <bizukin@yandex.ru>
2024-09-23 20:05:08 +0300

    формат Наценки

--- Prices_Tiles_Competitors/price_analyzes_v03.py
+++ Prices_Tiles_Competitors/price_analyzes_v03.py
@@ -52,7 +52,6 @@ for row in range(1, num_rows): #for row in range(1, num_rows):
             print(f"Произошла ошибка при поиске элемента: {type(e).__name__}: {e}")
             price = ""  # Присваиваем пустую строку вместо False
 
-        print(f"Получена цена: {price}")
         # Проверяем, является ли price строкой
         if isinstance(price, str):
             extracted_price = extract_price(price)
@@ -61,18 +60,33 @@ for row in range(1, num_rows): #for row in range(1, num_rows):
         else:
             df.iloc[row, col] = 0.0  # Или любое другое значение, если price не строка
 
-        natsenka = (extracted_price / zakup - 1) * 100
+        natsenka = (extracted_price / zakup - 1)
         print(f"Наценка: {natsenka}")
         df.iloc[row, col+1] = natsenka
 
 df = df.drop(df.columns[[1, 2]], axis=1)  # Удаляет колонки с индексами 1 и 2
 
+output_file = 'Prices_xlsx.xlsx'
 try:
-    df.to_excel('Prices_xlsx.xlsx', index=False, engine='openpyxl')
-    print("Данные успешно сохранены в файл 'Prices_xls.xlsx'")
+    df.to_excel(output_file, index=False, engine='openpyxl')
+    print(f"Данные успешно сохранены в файл '{output_file}'")
 except:
-    print("Ошибка записи в файл 'Prices_xlsx.xlsx' - не открыт ли файл?")
+    print(f"Ошибка записи в файл '{output_file}' - не открыт ли файл?")
 
+# Открываем файл для изменения формата
+wb = openpyxl.load_workbook(output_file)
+ws = wb.active
+
+# Устанавливаем формат процентов для колонок, где записывается natsenka
+for col in range(3, num_columns, 2):
+    for row in range(2, num_rows + 2):  # начинаем с 2, так как 1 - заголовки
+        ws.cell(row=row, column=col).number_format = '0.00%'  # Замените num_columns на индекс колонки natsenka
+
+try:
+    wb.save(output_file)
+    print(f"Данные успешно сохранены в файл '{output_file}'")
+except Exception as e:
+    print(f"Ошибка записи в файл 'Prices_xlsx.xlsx': {e}")
 
 browser.quit()
 
