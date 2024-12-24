f3e8299c719c287d0684b996cdc32ddf5143a77a
Aleksey <bizukin@yandex.ru>
2024-12-24 21:49:49 +0300

    parsing1

--- Parsing_characteristis_and_photo/characteristics_tile_parsing.py
+++ Parsing_characteristis_and_photo/characteristics_tile_parsing.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import openpyxl
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Настройте путь к вашему WebDriver
 webdriver_path = r"C:\Users\Administrator\Documents\install\chromedriver\chromedriver.exe"  # Замените на путь к вашему драйверу
@@ -26,27 +28,40 @@ base_url = "https://3dplitka.ru/"  # Замените на URL вашего са
 
 try:
     # Проходим по всем строкам таблицы
-    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
-        product_name = row[0].value  # Название товара из колонки "Товары"
+    for row in sheet.iter_rows(min_row=42, max_row=sheet.max_row):
+        brand = row[4].value  # Значение из 5-го столбца ("Brand")
+        collection = row[5].value  # Значение из 6-го столбца ("Collection")
 
-        if not product_name:
+        # Проверяем, что значения не пустые
+        if not brand and not collection:
             continue
 
-        print(f"Ищем товар: {product_name}")
+        brand_and_collection = brand + " " + collection
+        print(f"Ищем: {brand_and_collection}")
+
 
         # Открываем сайт и выполняем поиск товара
         driver.get(base_url)
-        search_box = driver.find_element(By.NAME, "q")  # Найдите селектор для поля поиска
-        search_box.clear()
-        search_box.send_keys(product_name)
+        search_box = driver.find_element(By.CLASS_NAME, "el-input__inner")  # Найдите селектор для поля поиска
+        print(search_box.tag_name)  # Должно быть "input"
+        search_box.send_keys(Keys.CONTROL + "a")  # Выделяем весь текст
+        search_box.send_keys(Keys.DELETE)  # Удаляем выделенный текст
+        search_box.send_keys(brand_and_collection)
         search_box.send_keys(Keys.RETURN)
 
-        time.sleep(3)  # Ожидание загрузки результатов
+        time.sleep(8)  # Ожидание загрузки результатов
 
         try:
+            element = driver.find_element(By.XPATH, "//li[starts-with(@id, 'el-autocomplete-7611-item-')]")
+            link = element.find_element(By.TAG_NAME, "a")
+            href = link.get_attribute("href")
+            print(f"Ссылка на товар: {href}")
+            link.click()
+
             # Открываем первую ссылку на найденный товар
-            product_link = driver.find_element(By.CSS_SELECTOR, "a.product-link")  # Замените на нужный селектор
-            product_link.click()
+            # product_link = driver.find_element(By.CSS_SELECTOR, "a[data-v-f2647c94]")  # Замените на нужный селектор
+            # print(f"product_link: {product_link}")
+            # product_link.click()
 
             time.sleep(3)  # Ожидание загрузки страницы товара
 
