cc3e72fb4856c23982af5960bec4e87b3c21ff02
Aleksey <bizukin@yandex.ru>
2025-01-08 20:09:28 +0300

    1

--- Parsing_characteristis_and_photo/parsing_3dplitka_07.py
+++ Parsing_characteristis_and_photo/parsing_3dplitka_07.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 # from selenium.webdriver.common.action_chains import ActionChains
 import openpyxl
 from openpyxl import load_workbook
