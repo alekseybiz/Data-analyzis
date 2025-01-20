23e2ea96016e7099462bade19ae7c4bdd9a951ba
Aleksey <bizukin@yandex.ru>
2025-01-20 16:10:50 +0300

    1

--- Parsing_characteristis_and_photo/parsing_Ozon_01.py
+++ Parsing_characteristis_and_photo/parsing_Ozon_01.py
@@ -18,7 +18,7 @@ openai.api_key = api_key
 client = OpenAI(api_key=api_key)  # This is the default and can be omitted
 
 # Настройте путь к вашему файлу Excel
-excel_path = "Ozon/ОЗОН Equipe_try.xlsx"
+excel_path = "Ozon/Озон Керамин.xlsx"
 
 # Функция для нахождения характеристик от ChatGPT
 def get_description(product_name, property, variants, explanation):
@@ -61,8 +61,8 @@ def get_description(product_name, property, variants, explanation):
 workbook = openpyxl.load_workbook(excel_path)
 sheet = workbook.active
 
-# col_numbers = [9, 10, 11, 12, 19, 20, 21, 22, 23, 24, 25, 28, 31, 32, 33, 34, 35, 36, 40, 43, 45, 46, 47, 48, 49, 50, 51, 52]
-col_numbers = [36]
+col_numbers = [9, 10, 11, 12, 19, 20, 21, 22, 23, 24, 25, 28, 31, 32, 33, 34, 35, 36, 40, 43, 45, 46, 47, 48, 49, 50, 51, 52]
+# col_numbers = [36]
 
 
 # Проходим по всем строкам таблицы
