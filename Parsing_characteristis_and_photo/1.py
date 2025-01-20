b2579339bc85343ca96de5bf6571c2788a191f05
Aleksey <bizukin@yandex.ru>
2025-01-20 18:44:50 +0300

    1

--- Parsing_characteristis_and_photo/1.py
+++ Parsing_characteristis_and_photo/1.py
@@ -1,14 +1,4 @@
-import openpyxl
+import torch
+print(torch.cuda.is_available())
 
-# Настройте путь к вашему файлу Excel
-excel_path = "Ozon/ОЗОН Equipe_try.xlsx"
-# Открываем таблицу Excel
-workbook = openpyxl.load_workbook(excel_path)
-sheet = workbook.active
-
-row_number = 6
-
-value = sheet.cell(row=row_number, column=36).value
-
-print(f"значение: {value}")
 
--- Parsing_characteristis_and_photo/watemark_remover_02.py
+++ Parsing_characteristis_and_photo/watemark_remover_02.py
@@ -2,13 +2,19 @@ from diffusers import StableDiffusionInpaintPipeline
 import torch
 from PIL import Image
 
-# Загрузка модели
-pipe = StableDiffusionInpaintPipeline.from_pretrained("runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+# Загружаем модель
+# pipe = StableDiffusionInpaintPipeline.from_pretrained("runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16)
+# pipe = pipe.to("cuda")  # Используйте "cpu", если GPU недоступен
 
-# Загрузка изображения
+# Загрузка модели без float16
+pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    "runwayml/stable-diffusion-inpainting"
+)
+pipe = pipe.to("cpu")
+
+# Загрузка изображения и маски
 image = Image.open("new-trend-jast-60120jas11p-beige-polirovannyij-60x120-sm-plitka.5b2a4d059c77.jpg").convert("RGB")
-mask = Image.open("w3dplitka_watermark_mask.png").convert("RGB")  # Белые области указывают на водяной знак
+mask = Image.open("watermark_mask.jpg").convert("RGB")  # Белые области указывают на водяной знак
 
 # Удаление водяного знака
 result = pipe(prompt="Remove watermark", image=image, mask_image=mask).images[0]
