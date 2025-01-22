09288edcd476cbe76bf9775a255b18082f468fcb
Aleksey <bizukin@yandex.ru>
2025-01-22 22:23:21 +0300

    1

--- Parsing_characteristis_and_photo/parsing_Ozon_01.py
+++ Parsing_characteristis_and_photo/parsing_Ozon_01.py
@@ -61,7 +61,7 @@ def get_description(product_name, property, variants, explanation):
 workbook = openpyxl.load_workbook(excel_path)
 sheet = workbook.active
 
-col_numbers = [9, 10, 11, 12, 19, 20, 21, 22, 23, 24, 25, 28, 31, 32, 33, 34, 35, 36, 40, 43, 45, 46, 47, 48, 49, 50, 51, 52]
+col_numbers = [9, 10, 11, 12, 19, 20, 21, 22, 23, 24, 25, 28, 31, 32, 33, 34, 35, 36, 40, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54]
 # col_numbers = [36]
 
 
