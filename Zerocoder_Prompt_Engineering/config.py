224a3bb816e4fd9aa8eeafc32945b7f17c43a92e
Aleksey <aleksey1.bizyukin@gmail.com>
2025-02-25 12:04:07 +0300

    1

--- Zerocoder_Prompt_Engineering/config.py
+++
@@ -1,4 +0,0 @@
-api_url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-api_key = "AQVNwqblv58OyJDVxOjpipaRWvNstY-MaAYCA0oX"
-# Авторизация в GigaChat:
-authorization_key = "YWJkMDc5N2QtMzA3OC00Yjg3LWE2Y2EtYmQ3MzRkNDAxYTBiOjg4NjE3ZTQyLTg5NzktNGM3Ny1iY2EyLTM0ODQ5ZTNjZWZiNA=="
